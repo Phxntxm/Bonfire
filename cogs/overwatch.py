3175f66bc5683a13b9429a2bff592fb23b55c296
Phxntxm <danielman0@yahoo.com>
2016-07-14 07:31:53 -0500

    Only added hero_stats if a hero is searched for

--- cogs/overwatch.py
+++ cogs/overwatch.py
@@ -51,8 +51,9 @@ class Overwatch:
         else:
             g_stats = data['general_stats']
             
-        fmt = "\n".join("{}: {}".format(i, r) for i, r in data['hero_stats'].items())
-        fmt += "\n".join("{}: {}".format(i, r) for i, r in g_stats if i in check_g_stats)
+        fmt = "\n".join("{}: {}".format(i, r) for i, r in g_stats if i in check_g_stats)
+        if hero not == "":
+            fmt += "\n".join("{}: {}".format(i, r) for i, r in data['hero_stats'].items())
         fmt += "\n".join("{}: {}".format(i, r) for i, r in o_stats if i in check_o_stats)
 
         #fmt = "Kills: {}".format(int(g_stats['eliminations']))
