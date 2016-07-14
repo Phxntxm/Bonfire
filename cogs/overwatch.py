3e4259307ac39de750c85a66ec56ea1e807eb680
Phxntxm <danielman0@yahoo.com>
2016-07-14 07:32:16 -0500

    Corrected syntax error

--- cogs/overwatch.py
+++ cogs/overwatch.py
@@ -52,7 +52,7 @@ class Overwatch:
             g_stats = data['general_stats']
             
         fmt = "\n".join("{}: {}".format(i, r) for i, r in g_stats if i in check_g_stats)
-        if hero not == "":
+        if not hero == "":
             fmt += "\n".join("{}: {}".format(i, r) for i, r in data['hero_stats'].items())
         fmt += "\n".join("{}: {}".format(i, r) for i, r in o_stats if i in check_o_stats)
 
