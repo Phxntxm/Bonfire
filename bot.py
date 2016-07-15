96fc8108181098007dc26b422c4f64e588ded327
Phxntxm <danielman0@yahoo.com>
2016-07-15 10:46:11 -0500

    Printing full traceback, as I can't find my error at all

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def on_command_error(error, ctx):
         await bot.send_message(ctx.message.channel, fmt)
     else:
         fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
-        await bot.send_message(ctx.message.channel,traceback.format_exc())
+        await bot.send_message(ctx.message.channel,str(traceback.format_exc()))
         #await bot.send_message(ctx.message.channel, fmt.format(type(error).__name__, error))
 
 if __name__ == '__main__':
