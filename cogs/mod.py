e5568740af0934bd1214807b53220622e5e5bf76
Phxntxm <danielman0@yahoo.com>
2016-07-16 07:00:49 -0500

    Fixed syntax error

--- cogs/mod.py
+++ cogs/mod.py
@@ -98,7 +98,7 @@ class Mod:
                                                                                                  command))
 
     @perms.command(name="add", aliases=["setup,create"], pass_context=True, no_pm=True)
-    @checks.has_permissions("manage_server")
+    @commands.has_permissions("manage_server")
     async def add_perms(self, ctx, *msg: str):
         """Sets up custom permissions on the provided command
         Format must be 'perms add <command> <permission>'"""
