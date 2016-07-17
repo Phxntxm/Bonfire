1c20ef2754346426c9e0f115b1892037dd946ac9
Phxntxm <danielman0@yahoo.com>
2016-07-17 13:20:44 -0500

    Printing full traceback for error

--- bot.py
+++ bot.py
@@ -57,6 +57,10 @@ async def on_command_error(error, ctx):
         await bot.send_message(ctx.message.channel, fmt)
     elif isinstance(error, commands.CheckFailure):
         fmt = "You can't tell me what to do!"
+        f = open("/home/phxntx5/public_html/Bonfire/error_log", 'w')
+        print('In {0.command.qualified_name}:'.format(ctx), file=f)
+        traceback.print_tb(error.original.__traceback__, file=f)
+        print('{0.__class__.__name__}: {0}'.format(error.original), file=f)
         await bot.send_message(ctx.message.channel, fmt)
     #elif isinstance(error, commands.CommandInvokeError):
         #f = open("/home/phxntx5/public_html/Bonfire/error_log", 'w')
--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -10,10 +10,6 @@ with open("/home/phxntx5/public_html/Bonfire/config.yml", "r") as f:
 
 connection = None
 
-db_default = global_config.get("db_default")
-db_boops = global_config.get("db_boops")
-db_perms = global_config.get("db_perms")
-
 botDescription = global_config.get("description")
 commandPrefix = global_config.get("command_prefix")
 
@@ -21,12 +17,6 @@ battleWins = global_config.get("battleWins", [])
 defaultStatus = global_config.get("default_status", "")
 botToken = global_config.get("bot_token", "")
 ownerID = global_config.get("owner_id", "")
-
-modCommands = global_config.get("modCommands", {})
-adminCommands = global_config.get("adminCommands", {})
-openCommands = global_config.get("openCommands", {})
-ownerCommands = global_config.get("ownerCommands", {})
-voiceCommands = global_config.get("voiceCommands", {})
 
 
 def getCursor():
