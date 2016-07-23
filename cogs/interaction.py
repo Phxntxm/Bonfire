ff27da7da0dd3264b549c29c769ad3cbe68052b7
Phxntxm <danielman0@yahoo.com>
2016-07-23 13:42:31 -0500

    Changed Timer to use asyncio's call_later method

--- cogs/interaction.py
+++ cogs/interaction.py
@@ -79,8 +79,9 @@ class Interaction:
         
         fmt = "{0.mention} has challenged you to a battle {1.mention}\n!accept or !decline"
         await self.bot.say(fmt.format(ctx.message.author, player2))
-        t = Timer(5, battlingOff, 'Thread-battle_off', [ctx.message.author.id])
-        t.start()
+        config.loop.call_later(5,battlingOff,ctx.message.author.id)
+        #t = Timer(5, battlingOff, 'Thread-battle_off', [ctx.message.author.id])
+        #t.start()
 
     @commands.command(pass_context=True, no_pm=True)
     @checks.customPermsOrRole("none")
