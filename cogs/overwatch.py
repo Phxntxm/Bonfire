fb7edc45236952afd59c8aa5f1e18fa584d439e2
Phxntxm <danielman0@yahoo.com>
2016-07-24 09:03:33 -0500

    Corrected syntax error

--- cogs/overwatch.py
+++ cogs/overwatch.py
@@ -41,7 +41,7 @@ class Overwatch:
 
         
         if hero == "":
-            await aiohttp.ClientSession() as s:
+            with aiohttp.ClientSession() as s:
                 async with s.get(base_url + "{}/stats/general".format(bt)) as r:
                     result = await r.text()
             
@@ -53,7 +53,7 @@ class Overwatch:
                 "Overwatch stats for {}: ```py\n{}```".format(user.name, fmt.title().replace("_", " ")))
         else:
             url = base_url + "{}/heroes/{}".format(bt, hero.lower().replace('-', '')
-            await aiohttp.ClientSession() as s:
+            with aiohttp.ClientSession() as s:
                 async with s.get(url) as r:
                     if r.status == 500
                         fmt = "{} has not used the hero {} before!".format(user.name, hero.title())
@@ -80,7 +80,7 @@ class Overwatch:
         await self.bot.say("Looking up your profile information....")
         url = base_url + "{}/stats/general".format(bt)
         
-        await aiohttp.ClientSession() as s:
+        with aiohttp.ClientSession() as s:
             async with s.get(url) as r:
                 if not r.status == 200:
                     await self.bot.say("Profile does not exist! Battletags are picky, "
