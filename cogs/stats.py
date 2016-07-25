2764730b8f425656a7f31a7879aad2dd3daea40d
Phxntxm <danielman0@yahoo.com>
2016-07-25 14:57:47 -0500

    Provided a stats command that should provide the battling stats for a specific user

--- cogs/stats.py
+++ cogs/stats.py
@@ -57,7 +57,7 @@ class Stats:
         
         server_member_ids = [member.id for member in ctx.message.server.members]
         server_members = {member_id:stats for member_id,stats in battles.items() if member_id in server_member_ids}
-        sorted_members = sorted(battles.items(), key = lambda x: x[1]['rating'],reverse=True)
+        sorted_members = sorted(server_members.items(), key = lambda x: x[1]['rating'],reverse=True)
         
         fmt = ""
         count = 1
@@ -68,7 +68,30 @@ class Stats:
             fmt += "#{}) {} (Rating: {})\n".format(count,member.display_name,stats.get('rating')) 
             count += 1
         await self.bot.say("```{}```".format(fmt))
-
+        
+    
+    @commands.command(pass_context=True)
+    @checks.customPermsOrRole("send_messages")
+    asynf def stats(self, ctx, member: discord.Member=None)
+        """Prints the battling stats for you, or the user provided"""
+        member = member or ctx.message.author
+        
+        all_members = config.getContent('battle_records')
+        if not member in all_members:
+            await self.bot.say("That user has not battled yet!")
+            return
+             
+        server_member_ids = [member.id for member in ctx.message.server.members]
+        server_members = {member_id:stats for member_id,stats in all_members.items() if member_id in server_member_ids}
+        sorted_server_members = sorted(server_members.items(), key = lambda x: x[1]['rating'],reverse=True)
+        sorted_all_members = sorted(all_members.items(), key = lambda x: x[1]['rating'],reverse=True)
+        
+        server_rank = [i for i,x in enumerate(sorted_server_members) if x[0] == member.id][0]
+        total_rank = [i for i,x in enumerate(sorted_all_members) if x[0] == member.id][0]
+        rating = server_members[ctx.message.author.id]['rating']
+        record = "{}-{}".format(server_members[member.id]['wins'],server_members[member.id]['losses'])
+        fmt = 'Stats for {}:\n\tRecord: {}\n\tServer Rank: {}\n\tOverall Rank: {}\n\tRating: {}'
+        .format(member.display_name,record,server_rank,total_rank,rating)
 
 def setup(bot):
     bot.add_cog(Stats(bot))
