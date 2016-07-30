945e3ef8784e6dc8cadff05347753f7804ca0a8e
Phxntxm <danielman0@yahoo.com>
2016-07-30 10:00:51 -0500

    corrected syntax error

--- cogs/core.py
+++ cogs/core.py
@@ -65,7 +65,7 @@ class Core:
     @checks.customPermsOrRole(send_messages=True)
     async def uptime(self):
         """Provides a printout of the current bot's uptime"""
-        awayt self.bot.say("Uptime = ```{}```".format(get_bot_uptime()))
+        await self.bot.say("Uptime = ```{}```".format(get_bot_uptime()))
 
     @commands.command()
     @checks.customPermsOrRole(send_messages=True)
