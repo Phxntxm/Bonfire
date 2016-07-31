2b4d331b1a017eb6bfa53caa3cb21aa0ebc2c5e0
phxntxm <danielman0@yahoo.com>
2016-07-30 21:16:59 -0500

    Corrected issue where the comparison for custom permissions was comparing the wrong value

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -18,7 +18,9 @@ def customPermsOrRole(**perms):
             setattr(default_perms, perm, setting)
 
         try:
-            required_perm = config.getContent('custom_permissions')[ctx.message.server.id][ctx.command.qualified_name]
+            required_perm_value = config.getContent('custom_permissions')[ctx.message.server.id][
+                ctx.command.qualified_name]
+            required_perm = discord.Permissions(required_perm_value)
         except KeyError:
             required_perm = default_perms
         return member_perms >= required_perm
