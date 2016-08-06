1cea255d8342fd7131a7084e3fa2f6defea839f3
phxntxm <danielman0@yahoo.com>
2016-08-06 17:20:23 -0500

    Pep8 updates

--- cogs/interaction.py
+++ cogs/interaction.py
@@ -8,12 +8,12 @@ import random
 
 def battlingOff(player_id):
     battling = config.getContent('battling')
-    
-    battling = {p1:p2 for p1,p2 in battling.items() if not p2 == player_id and not p1 == player_id}
-    
-    config.saveContent('battling',battling)
-                
-    
+
+    battling = {p1: p2 for p1, p2 in battling.items() if not p2 == player_id and not p1 == player_id}
+
+    config.saveContent('battling', battling)
+
+
 def userBattling(ctx):
     battling = config.getContent('battling')
     if battling is None:
@@ -22,7 +22,7 @@ def userBattling(ctx):
         return True
     if str(ctx.command) == 'battle':
         return ctx.message.mentions[0].id in battling.values() or ctx.message.mentions[0].id in battling.keys()
-        
+
     return False
 
 
@@ -30,13 +30,13 @@ def updateBattleRecords(winner, loser):
     battles = config.getContent('battle_records')
     if battles is None:
         battles = {winner.id: "1-0", loser.id: "0-1"}
-    
+
     winner_stats = battles.get(winner.id) or {}
     winner_rating = winner_stats.get('rating') or 1000
-    
+
     loser_stats = battles.get(loser.id) or {}
     loser_rating = loser_stats.get('rating') or 1000
-        
+
     difference = abs(winner_rating - loser_rating)
     rating_change = 0
     count = 25
@@ -45,26 +45,26 @@ def updateBattleRecords(winner, loser):
             break
         rating_change += 1
         count += 25
-    
+
     if winner_rating > loser_rating:
         winner_rating += 16 - rating_change
         loser_rating -= 16 - rating_change
     else:
         winner_rating += 16 + rating_change
         loser_rating -= 16 + rating_change
-    
+
     winner_wins = winner_stats.get('wins') or 0
     winner_losses = winner_stats.get('losses') or 0
     loser_wins = loser_stats.get('wins') or 0
     loser_losses = loser_stats.get('losses') or 0
     winner_wins += 1
     loser_losses += 1
-    
-    winner_stats = {'wins':winner_wins,'losses':winner_losses,'rating':winner_rating}
-    loser_stats = {'wins':loser_wins,'losses':loser_losses,'rating':loser_rating}
+
+    winner_stats = {'wins': winner_wins, 'losses': winner_losses, 'rating': winner_rating}
+    loser_stats = {'wins': loser_wins, 'losses': loser_losses, 'rating': loser_rating}
     battles[winner.id] = winner_stats
     battles[loser.id] = loser_stats
-    
+
     return config.saveContent('battle_records', battles)
 
 
@@ -74,8 +74,8 @@ class Interaction:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True, no_pm=True,invoke_without_command=True)
-    @commands.cooldown(1,180,BucketType.user)
+    @commands.group(pass_context=True, no_pm=True, invoke_without_command=True)
+    @commands.cooldown(1, 180, BucketType.user)
     @checks.customPermsOrRole(send_messages=True)
     async def battle(self, ctx, player2: discord.Member):
         """Challenges the mentioned user to a battle"""
@@ -94,16 +94,16 @@ class Interaction:
         if userBattling(ctx):
             await self.bot.say("You or the person you are trying to battle is already in a battle!")
             return
-            
+
         battling = config.getContent('battling') or {}
         battling[ctx.message.author.id] = ctx.message.mentions[0].id
         config.saveContent('battling', battling)
-        
+
         fmt = "{0.mention} has challenged you to a battle {1.mention}\n!accept or !decline"
         await self.bot.say(fmt.format(ctx.message.author, player2))
         await self.bot.delete_message(ctx.message)
-        config.loop.call_later(180,battlingOff,ctx.message.author.id)
-        
+        config.loop.call_later(180, battlingOff, ctx.message.author.id)
+
     @commands.command(pass_context=True, no_pm=True)
     @checks.customPermsOrRole(send_messages=True)
     async def accept(self, ctx):
@@ -111,25 +111,25 @@ class Interaction:
         if not userBattling(ctx):
             await self.bot.say("You are not currently in a battle!")
             return
-            
+
         battling = config.getContent('battling') or {}
-        p1 = [p1_id for p1_id,p2_id in battling.items() if p2_id == ctx.message.author.id]
+        p1 = [p1_id for p1_id, p2_id in battling.items() if p2_id == ctx.message.author.id]
         if len(p1) == 0:
             await self.bot.say("You are not currently being challenged to a battle!")
             return
-            
-        battleP1 = discord.utils.find(lambda m: m.id == p1[0],ctx.message.server.members)
+
+        battleP1 = discord.utils.find(lambda m: m.id == p1[0], ctx.message.server.members)
         battleP2 = ctx.message.author
-        
+
         fmt = config.battleWins[random.randint(0, len(config.battleWins) - 1)]
-        
+
         if random.randint(1, 100) < 50:
             await self.bot.say(fmt.format(battleP1.mention, battleP2.mention))
             updateBattleRecords(battleP1, battleP2)
         else:
             await self.bot.say(fmt.format(battleP2.mention, battleP1.mention))
             updateBattleRecords(battleP2, battleP1)
-        
+
         await self.bot.delete_message(ctx.message)
         battlingOff(ctx.message.author.id)
 
@@ -140,15 +140,15 @@ class Interaction:
         if not userBattling(ctx):
             await self.bot.say("You are not currently in a battle!")
             return
-            
+
         battling = config.getContent('battling') or {}
-        p1 = [p1_id for p1_id,p2_id in battling.items() if p2_id == ctx.message.author.id]
+        p1 = [p1_id for p1_id, p2_id in battling.items() if p2_id == ctx.message.author.id]
         if len(p1) == 0:
             await self.bot.say("You are not currently being challenged to a battle!")
             return
-        battleP1 = discord.utils.find(lambda m: m.id == p1[0],ctx.message.server.members)
+        battleP1 = discord.utils.find(lambda m: m.id == p1[0], ctx.message.server.members)
         battleP2 = ctx.message.author
-        
+
         await self.bot.say("{0} has chickened out! What a loser~".format(battleP2.mention, battleP1.mention))
         await self.bot.delete_message(ctx.message)
         battlingOff(ctx.message.author.id)
@@ -173,7 +173,7 @@ class Interaction:
             return
 
         boops = config.getContent('boops') or {}
-        
+
         amount = 1
         booper_boops = boops.get(ctx.message.author.id)
         if booper_boops is None:
--- cogs/statsupdate.py
+++ cogs/statsupdate.py
@@ -7,13 +7,14 @@ log = logging.getLogger()
 
 discord_bots_url = 'https://bots.discord.pw/api'
 
+
 class StatsUpdate:
     """This is used purely to update stats information for carbonitex and botx.discord.pw"""
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-    
+
     def __unload(self):
         config.loop.create_task(self.session.close())
 
@@ -30,7 +31,7 @@ class StatsUpdate:
         url = '{0}/bots/{1.user.id}/stats'.format(discord_bots_url, self.bot)
         async with self.session.post(url, data=payload, headers=headers) as resp:
             log.info('bots.discord.pw statistics returned {0.status} for {1}'.format(resp, payload))
-        
+
     async def on_server_join(self, server):
         await self.update()
 
@@ -39,6 +40,7 @@ class StatsUpdate:
 
     async def on_ready(self):
         await self.update()
+
 
 def setup(bot):
     bot.add_cog(StatsUpdate(bot))
