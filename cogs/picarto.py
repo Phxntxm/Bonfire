125eb1895c3727d789dfb087b474daa1e49f6bee
Phxntxm <danielman0@yahoo.com>
2016-08-08 16:46:02 -0500

    Changed when to title and replace _, as it was affecting more than I wanted

--- cogs/picarto.py
+++ cogs/picarto.py
@@ -54,12 +54,12 @@ class Picarto:
         
         data = json.loads(response)
         things_to_print = ['channel','commissions_enabled','is_nsfw','program','tablet','followers','content_type']
-        fmt = "\n".join("{}: {}".format(i, r) for i,r in data.items() if i in things_to_print)
+        fmt = "\n".join("{}: {}".format(i.title().replace("_", " "), r) for i,r in data.items() if i in things_to_print)
         social_links = data.get('social_urls')
         if social_links:
-            fmt2 = "\n".join("\t{}: {}".format(i, r) for i,r in social_links.items())
+            fmt2 = "\n".join("\t{}: {}".format(i.title().replace("_", " "), r) for i,r in social_links.items())
             fmt = "{}\nSocial Links:\n{}".format(fmt, fmt2)
-        await self.bot.say("Picarto stats for {}: ```\n{}```".format(member.display_name, fmt.title().replace("_", " ")))
+        await self.bot.say("Picarto stats for {}: ```\n{}```".format(member.display_name, fmt))
         
     @picarto.command(name='add', pass_context=True, no_pm=True)
     @checks.customPermsOrRole(send_messages=True)
