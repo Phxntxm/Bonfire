d16db79a79427522e12fef4729b77e0245162e6f
Phxntxm <danielman0@yahoo.com>
2016-10-07 15:46:11 -0500

    Added the exception checking for when no date is provided

--- cogs/core.py
+++ cogs/core.py
@@ -25,8 +25,12 @@ class Core:
         This will most likely not be updated every day, however messages will still be pushed to this every now and then"""
         if date is None:
             motd = await config.get_content('motd')
-            date = motd['date']
-            motd = motd['motd']
+            try:
+                date = motd['date']
+                motd = motd['motd']
+            # This one will be hit if we return None for that day
+            except TypeError:
+                await self.bot.say("No message of the day for {}!".format(date))
             fmt = "Last updated: {}\n\n{}".format(date, motd)
             await self.bot.say(fmt)
         else:
