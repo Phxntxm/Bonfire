286af4e356e8796590e31e1e6230b81ef49eb4ed
phxntxm <danielman0@yahoo.com>
2017-03-08 02:40:55 -0600

    Looked at the right key...

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -120,7 +120,7 @@ def command_prefix(bot, message):
     # But it is not worth a query for every single message the bot detects, to fix
     try:
         prefixes = cache['server_settings'].values
-        prefix = [x for x in prefixes if x['prefix'] == str(message.guild.id)][0]['prefix']
+        prefix = [x for x in prefixes if x['server_id'] == str(message.guild.id)][0]['prefix']
         return prefix or default_prefix
     except (KeyError, TypeError, IndexError, AttributeError, KeyError):
         return default_prefix
