ea62d899bfed78197c0317481794aff20c3a7f00
phxntxm <danielman0@yahoo.com>
2017-06-27 19:13:38 -0500

    Add the new check_restricted method

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,5 +1,5 @@
 from .cards import Deck
-from .checks import is_owner, custom_perms, db_check, should_ignore
+from .checks import is_owner, custom_perms, db_check, should_ignore, check_restricted
 from .config import *
 from .utilities import *
 from .images import create_banner
